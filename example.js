bd713077e76127f78712ae10981367bd23d3484f
David White <david.white@telegraph.co.uk>
2019-05-15 15:15:34 +0100

    Added test JSON

---
+++ example.js
@@ -0,0 +1,21 @@
+{
+    "total": 10,
+    "items": [
+    {
+            "url":"/tv/0/game-thrones-drogon-fears-easily-can-dragons-killed-could/",
+            "headline":"Game of Thrones Drogon injury: how easily can dragons be killed - and could Daenerys hatch more?",
+            "pubDate":"2017-08-07T06:25:07+0000",
+            "comments": false,
+            "image": {
+                    "path":"/content/dam/tv/2017/08/07/drogon_1_trans_NvBQzQNjv4BqfmWw8ZtkYoKgUQYLfpqhYv3Lyj9RShzlDBeus8lXwhc.jpg",
+                    "altText":"Game of Thrones: Drogon in Spoils of War"
+            },
+            "authors": [
+                {
+                    "name":"Rebecca Hawkes",
+                    "url":"/authors/rebecca-hawkes/"
+                }
+            ]
+        }
+    ]
+}
