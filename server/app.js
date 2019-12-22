71c560ecd978bd992f74c70566eaec44026df54e
Chris Boakes <chris@chrisboakes.com>
2019-12-22 15:48:01 +0000

    Routing and express-handlebars structure

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const router = require('./router');
 const app = express();
 const port = 3000;
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
 app.use(router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Listening on port ${port}!`));
--- server/router.js
+++ server/router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Hello World!'));
+router.get('/', (req, res) => {
+    res.render('home');
+});
 
 module.exports = router;
