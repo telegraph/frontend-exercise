ea7e1fa4f075afd5ffb9cf57d56e49b081da3873
Chris Boakes <chris@chrisboakes.com>
2019-12-23 11:59:53 +0000

    Added nodemon and proper watchers

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const router = require('./router');
+const meta = require("./content/meta.json");
 const app = express();
 const port = 3000;
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+app.set('meta', meta);
 
 app.use('*/static', express.static('public'));
 
